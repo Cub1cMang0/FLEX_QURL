da8e2a062364c60342c036012b9ea4689b2555fe
Gabriel <gsotelosil14@gmail.com>
2026-07-30 09:04:26 -0700

    Changed parameters in header file.

--- flex-cli/src/mainimageconverter.h
+++ flex-cli/src/mainimageconverter.h
@@ -3,13 +3,16 @@
 
 #include <QImage>
 #include "imagefileconverter.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 class MainImageConverter: public QObject, public ImageFileConverter
 {
     Q_OBJECT
     public:
         explicit MainImageConverter(QObject *parent = nullptr);
-        void convert_image(const QString &input_path, const QString &output_path, const QString &input_ext, const QString &output_ext);
+        void convert_image(const QString &input_path, const QString &output_path, const QString &input_ext, const QString &output_ext, const json &load_data);
     signals:
         void update_image_progress(const QString &message, bool success);
 };
